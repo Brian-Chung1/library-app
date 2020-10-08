3370c34e88fe8e6ead7e4f357d99771d4e6e72e7
Brian-Chung1 <brian.chung.cs@gmail.com>
2020-10-07 17:39:56 -0700

    added buttons for editting and trashing

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const booklist = document.querySelector(".book-list");
 const addbookButton = document.querySelector("#addbook");
 const saveButton = document.querySelector(".save");
+const editButton = document.querySelector(".edit");
+const trashButton = document.querySelector(".trash");
+let readBoolean = false;
 const library = [];
 
 
@@ -10,6 +13,7 @@ const author = document.querySelector("input[name='author']");
 const pages = document.querySelector("input[name='pages']");
 const yesButton = document.querySelector("#yes");
 const noButton = document.querySelector("#no");
+const selectButton = document.querySelector("#select");
 //------------------------------------------------------------
 
 class Book {
@@ -25,6 +29,16 @@ class Book {
 
 
 }
+
+yesButton.addEventListener("click", () => {
+    readBoolean = true;
+    selectButton.textContent = "Yes";
+})
+
+noButton.addEventListener("click", () => {
+    readBoolean = false;
+    selectButton.textContent = "No";
+})
 
 function newBook(title, author, pages, read) {
     let b = new Book(title, author, pages, read);
@@ -50,23 +64,41 @@ function createBookElement(title, author, pages, read) {
     const bookAuthorElement = document.createElement("h2");
     const bookPageElement = document.createElement("h4");
     const bookFinishedElement = document.createElement("h4");
+    const editElement = document.createElement("img");
+    const trashElement = document.createElement("img");
+    editElement.src = "images/edit-regular.png";
+    trashElement.src = "images/trash-alt-solid.png";
 
     //Book text content
-    bookTitleElement.textContent = "Example Book";
-    bookAuthorElement.textContent = "By Author";
-    bookPageElement.textContent = "Page Count: 1000";
-    bookFinishedElement.textContent = "Finished Reading";
+    bookTitleElement.textContent = title;
+    if(author !== "") {
+        bookAuthorElement.textContent = `By ${author}`;
+    }
+    if(pages !== "") {
+        bookPageElement.textContent = `${pages} Pages`;
+    }
+    let readString;
+    if(read) {
+        readString = "Finished Reading"
+    } else {
+        readString = "Have Not Finished Reading"
+    }
+    bookFinishedElement.textContent = readString;
 
     //Book styles
     bookContent.classList.add("card-header");
     newBookElement.classList.add("book");
     bookTitleElement.classList.add("book-title");
+    trashElement.classList.add("trash");
+    editElement.classList.add("edit");
 
     //Appending Book DOM nodes
     bookContent.appendChild(bookTitleElement);
     bookContent.appendChild(bookAuthorElement);
     bookContent.appendChild(bookPageElement);
     bookContent.appendChild(bookFinishedElement);
+    bookContent.appendChild(trashElement);
+    bookContent.appendChild(editElement);
     newBookElement.appendChild(bookContent);
     booklist.insertBefore(newBookElement, addbookButton);
 
@@ -75,10 +107,21 @@ function createBookElement(title, author, pages, read) {
 }
 
 saveButton.addEventListener("click", () => {
-    createBookElement();
+    if(title.value == "") {
+        alert("Please enter a book title");
+    } else {
+        createBookElement(title.value, author.value, pages.value, readBoolean);
+    } 
+    
 })
 
-
+function reset() {
+    title.value = "";
+    author.value = "";
+    pages.value = "";
+    readBoolean = false;
+    selectButton.textContent = "Select";
+}
 
 
 
@@ -100,6 +143,7 @@ function closeModal(modal) {
     if(modal == null) return;
     modal.classList.remove("active");
     overlay.classList.remove("active");
+    reset();
 }
 
 openModalButtons.forEach(button => {
